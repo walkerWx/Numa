073057f0f6284aa165ead750dc77382fd40f2664
Walker <walkernju@gmail.com>
2015-03-22 14:40:20 +0800

    add test files

---
+++ test/tc_compute.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+using namespace std;
+
+class X {
+};
+
+int compute() { 
+	return 0;
+}
+
+static std::string foo() {
+  return "foo";
+}
+
+int main(){
+  return 0;
+}
+
---
+++ test/tc_noinclude.cpp
@@ -0,0 +1,5 @@
+int main(){
+  double x = 1.0;
+  x = x + 1;
+  return 0;
+}
